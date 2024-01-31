c4658224e2c82c349e22bdea6abdd58c76ebfc98
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-01-31 23:14:49 +0600

    models.py

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Channel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='channel')
+    subscribers = models.ManyToManyField(User, related_name='subscribed_channels', blank=True)
+
+    def __str__(self):
+        return self.name
--- apps/videos/models.py
+++ apps/videos/models.py
@@ -1,4 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.timezone import now
+
+from apps.user.models import Channel
+
 
 # Create your models here.
 
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    video_file = models.FileField(upload_to='videos/')
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, related_name='channel')
+    created_date = models.DateTimeField(blank=False, default=now, editable=True)
+
+
+class Like(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='likes')
+
+    def __str__(self):
+        return f"Like from {self.author} to {self.post}"
