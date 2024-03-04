7f7ce810df1eb9f74d1336bfffb1be7b3b7a546a
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-03-04 18:11:23 +0600

    added video

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/users/", include("apps.user.urls")),
+    path("api-auth/", include("apps.user.urls")),
     path("api/v1/", include("apps.videos.urls")),
 ]
